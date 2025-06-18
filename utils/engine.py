9ebec4c9fef48f8c942795418afd18c264ccc49c
Gabe <g.francke.tech@gmail.com>
2025-06-18 19:43:47 +0200

    feat: add train function for model training and testing

--- utils/engine.py
+++ utils/engine.py
@@ -101,3 +101,16 @@ def test_step(model: torch.nn.Module,
     test_acc = test_acc / len(dataloader)
 
     return test_loss, test_acc
+
+def train(model: torch.nn.Module,
+          train_dataloader: torch.utils.data.DataLoader,
+          test_dataloader: torch.utils.data.DataLoader,
+          loss_fn: torch.nn.Module,
+          optimizer: torch.optim.Optimizer,
+          epochs: int,
+          device: torch.device) -> Dict[str, List[float]]:
+    """Trains and tests a PyTorch model.
+
+    """
+
+    pass # TODO: Implement the main training loop with tqdm progress bar
