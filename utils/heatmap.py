95598cc159274f9b29d5e8f079e2400ce01f0997
Gabe <g.francke.tech@gmail.com>
2025-07-14 20:53:22 +0200

    fix: add opencv-python to requirements and remove normalization from load_image function

--- utils/heatmap.py
+++ utils/heatmap.py
@@ -10,7 +10,6 @@ def load_image(image_path):
     img = cv2.resize(img, (224, 224))
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = transforms.ToTensor()(img)
-    img = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])(img)
     img = img.unsqueeze(0)
     return img
 
