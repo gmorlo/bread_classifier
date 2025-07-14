193604b647fd99c1b41ef8c000bb232f5eb67d89
Gabe <g.francke.tech@gmail.com>
2025-07-14 20:54:32 +0200

    feat: add get class label

--- utils/heatmap.py
+++ utils/heatmap.py
@@ -5,11 +5,16 @@ import torch
 import torchvision.transforms as transforms
 from torchvision import models
 
-def load_image(image_path):
+def load_image(image_path: str):
+    """Loads an image from a file path and preprocesses it for model input."""
     img = cv2.imread(image_path, cv2.IMREAD_COLOR)
     img = cv2.resize(img, (224, 224))
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = transforms.ToTensor()(img)
     img = img.unsqueeze(0)
     return img
+
+def get_class_label(preds: torch.Tensor) -> int:
+    class_idx = torch.argmax(preds, dim=1).item()
+    return class_idx
 
